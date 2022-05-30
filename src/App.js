deaeeea44e22f5118f2acab6784cd69e1558b094
Supawich Sitdhipol <Supawichs@gmail.com>
2022-05-30 21:15:00 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(100);
+  const [isOpen, setIsOpen] = useState(true);
+  const [temp, setTemp] = useState(0);
+  const [enter, setEnter] = useState(true);
+  const [exit, setExit] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='top'>
+          <div style={{ display: 'flex', flexDirection: 'row', padding: '0', margin: '0' }}>
+            <p className="status">Door Status: </p>
+            {isOpen ? <p className='open'>Open</p> : <p className='closed'>Closed</p>}
+          </div>
+          {enter && <p className="alert">A person entered with temperature of {temp} degrees</p>}
+          {exit && <p className="alert">A person exited</p>}
+        </div>
+        <div className="bottom">
+          <p className="count">
+            Number of people in the building: {count}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyABhoChx6NKsyMYm6HI4YLPHNERozh4kvo",
+    authDomain: "embedded-project-efe7b.firebaseapp.com",
+    projectId: "embedded-project-efe7b",
+    storageBucket: "embedded-project-efe7b.appspot.com",
+    messagingSenderId: "408956803749",
+    appId: "1:408956803749:web:cc1c8ddf6e0345fbe44b4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebase;
